2c4cddf08aa427fc060d16b7162d73a04ec8fd6e
Ahmad Karambakhsh <ahmad.karambakhsh@gmail.com>
2024-01-11 17:52:38 +0800

    update

--- test_DA3.py
+++ test_DA3.py
@@ -7,12 +7,35 @@ from ak_DataAssociation import ShiTomasiAndORB, BruteForceMatcher
 
 
 # this is your dataset path
-ds_path = r'DS\beethoven_data\images'
+ds_path = r'DS\diamond_walk\diamond_walk\cam0\data'
+K=[600,   0,   320,
+    0,     600, 240,
+    0,     0,   1]
+K=np.reshape(K,(3,3))
 
 sao = {}
 
 bf = BruteForceMatcher()
 
+def recoverpos(E):
+    # Singular Value Decomposition (SVD) of the essential matrix
+    U, _, Vt = np.linalg.svd(E)
+
+    # Ensure that the determinant of U and Vt is positive to make them rotation matrices
+    if np.linalg.det(U) < 0:
+        U *= -1
+    if np.linalg.det(Vt) < 0:
+        Vt *= -1
+
+    # Compute the rotation matrix R from U and Vt
+    R = np.dot(U, Vt)
+
+    # Compute the translation vector t
+    t = U[:, 2]
+
+    # Ensure that t is a unit vector
+    t /= np.linalg.norm(t)
+    return R,t
 # Loop through each file in the directory
 for index, filename in enumerate(os.listdir(ds_path)):
     if filename.lower().endswith(('.png', '.jpg', '.jpeg', '.tiff', '.bmp', '.gif', '.ppm')):
@@ -69,7 +92,7 @@ for index, filename in enumerate(os.listdir(ds_path)):
                 pts1 = np.float32([keypoints1[m.queryIdx].pt for m in good_matches])
                 pts2 = np.float32([keypoints2[m.trainIdx].pt for m in good_matches])
 
-                F, mask = cv2.findFundamentalMat(pts1, pts2, cv2.FM_RANSAC)
+                F, mask = cv2.findFundamentalMat(pts1, pts2, cv2.FM_RANSAC )
 
                 if F is None or F.shape != (3, 3):
                     print("Error in computing the fundamental matrix")
@@ -83,19 +106,27 @@ for index, filename in enumerate(os.listdir(ds_path)):
 
             inlier_keypoints1 = [cv2.KeyPoint(x=p[0], y=p[1], size=20) for p in inlier_pts1]
             inlier_keypoints2 = [cv2.KeyPoint(x=p[0], y=p[1], size=20) for p in inlier_pts2]
-            print('done')
+            # print('done')
 
             for pt1, pt2 in zip(inlier_pts1, inlier_pts2):
                 x = np.array([pt1[0], pt1[1], 1])
                 x_prime = np.array([pt2[0], pt2[1], 1])
                 error = np.dot(np.dot(x_prime.T, F), x)
-                print("Epipolar Constraint Error:", error)
+                # print("Epipolar Constraint Error:", error)
 
             image_with_keypoints = cv2.drawKeypoints(image_with_keypoints, inlier_keypoints2, None, color=(255, 255, 0), flags=0)
 
-            # E = np.dot(np.dot(K.T, F), K)
+            E = np.dot(np.dot(K.T, F), K) 
             
 
+            _, R, t, _ = cv2.recoverPose(E, inlier_pts1, inlier_pts2, K)
+            R1,t1 = recoverpos(E)
+            
+            print(R)
+            print(R1)
+            print(t)
+            print(t1)
+            
         # Display the image with keypoints
         cv2.imshow('Keypoints and Descriptors', image_with_keypoints)
         cv2.waitKey(0)
